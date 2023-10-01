dd46dba5bd8f5b566500581baccbcff285691b74
Saidul Mursalin <itzmonir@gmail.com>
2023-10-01 23:46:25 +0600

    Nav and Footer Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
     <footer className="bg-slate-950">
       <div className="max-w-screen-xl mx-auto py-8 px-3">
         <p className="text-center text-white">&copy; SimpleFireBaseAuth&trade; 2023. All right reserved.</p>
+        <p><Link to="/terms">Terms and Conditions</Link></p>
       </div>
     </footer>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,6 @@ const Navbar = () => {
     <li><NavLink to="/">Home</NavLink></li>
     <li><NavLink to="/login">Login</NavLink></li>
     <li><NavLink to="/register">Register</NavLink></li>
-    <li><NavLink to="/terms">Terms and Conditions</NavLink></li>
   </>
 
   return (
