d6bee098b17c1e6cd0fcd3c51b614d1cd15d733a
Saidul Mursalin <itzmonir@gmail.com>
2023-10-02 00:00:34 +0600

    Footer Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-slate-950">
       <div className="max-w-screen-xl mx-auto py-8 px-3">
         <p className="text-center text-white">&copy; SimpleFireBaseAuth&trade; 2023. All right reserved.</p>
-        <p className="text-center text-white flex gap-5"><Link to="/terms">Terms and Conditions</Link> <a href="https://github.com/rocktohq/simple-firebase-auth.git" rel="noreferrer" target="_blank">Source Code</a></p>
+        <p className="text-center text-white flex gap-5 justify-center"><Link to="/terms">Terms and Conditions</Link> <a href="https://github.com/rocktohq/simple-firebase-auth.git" rel="noreferrer" target="_blank">Source Code</a></p>
       </div>
     </footer>
   )
