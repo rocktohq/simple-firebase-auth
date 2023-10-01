d5f244f7a755b82cc576606c9cdfcba9410b9d47
Saidul Mursalin <itzmonir@gmail.com>
2023-10-01 23:53:50 +0600

    Footer Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { AiOutlineGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <footer className="bg-slate-950">
       <div className="max-w-screen-xl mx-auto py-8 px-3">
         <p className="text-center text-white">&copy; SimpleFireBaseAuth&trade; 2023. All right reserved.</p>
-        <p><Link to="/terms">Terms and Conditions</Link></p>
+        <p className="text-center"><Link to="/terms">Terms and Conditions</Link> <a className="ml-5" href="https://github.com/rocktohq/simple-firebase-auth.git" rel="noreferrer" target="_blank"><AiOutlineGithub></AiOutlineGithub> Source Code</a></p>
       </div>
     </footer>
   )
